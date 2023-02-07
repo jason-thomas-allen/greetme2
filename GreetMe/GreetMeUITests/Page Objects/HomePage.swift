96aebcdad49b1b9a244c66fb145913f52c33b38d
Jason Allen <jason.thomas.allen@protonmail.com>
2023-02-07 13:55:21 +0800

    empty page objects

---
+++ GreetMe/GreetMeUITests/Page Objects/HomePage.swift
@@ -0,0 +1,8 @@
+//
+//  HomePage.swift
+//  GreetMeUITests
+//
+//  Created by Jason Allen on 7/2/2023.
+//
+
+import Foundation
---
+++ GreetMe/GreetMeUITests/Page Objects/LoginPage.swift
@@ -0,0 +1,8 @@
+//
+//  LoginPage.swift
+//  GreetMeUITests
+//
+//  Created by Jason Allen on 7/2/2023.
+//
+
+import Foundation
