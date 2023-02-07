42d85d4549134aff50b692f71ab16151528946a9
Jason Allen <jason.thomas.allen@protonmail.com>
2023-02-07 14:23:43 +0800

    init views

--- GreetMe/GreetMeFramework/Views/HomeView.swift
+++ GreetMe/GreetMeFramework/Views/HomeView.swift
@@ -2,7 +2,26 @@
 //  HomeView.swift
 //  GreetMeFramework
 //
-//  Created by Jason Allen on 7/2/2023.
+//  Created by Jason Allen on 5/2/2023.
 //
 
-import Foundation
+import SwiftUI
+
+public struct HomeView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        VStack {
+            Text("Hello World")
+            .navigationTitle("Home")
+            .accessibilityIdentifier("greetingTextField")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- GreetMe/GreetMeFramework/Views/LoginView.swift
+++ GreetMe/GreetMeFramework/Views/LoginView.swift
@@ -2,7 +2,54 @@
 //  LoginView.swift
 //  GreetMeFramework
 //
-//  Created by Jason Allen on 7/2/2023.
+//  Created by Jason Allen on 5/2/2023.
 //
 
-import Foundation
+import SwiftUI
+
+public struct LoginView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        NavigationView {
+            
+            VStack {
+                Form {
+                    
+                    TextField("User name", text: .init(get: { "" }, set: { $0 }))
+                        .accessibilityIdentifier("usernameTextField")
+                    
+                    TextField("Password", text: .init(get: { "" }, set: { $0 }))
+                        .accessibilityIdentifier("passwordTextField")
+                    
+                    HStack {
+                        Spacer()
+                        Button {
+                            
+                        } label: {
+                            Text("Login")
+                                .accessibilityIdentifier("loginButton")
+                        }
+                        Spacer()
+                    }
+                }
+                
+                NavigationLink(isActive: .constant(false)) {
+                    HomeView()
+                } label: {
+                    EmptyView()
+                }
+                
+                
+            }
+            .navigationTitle("Login")
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
